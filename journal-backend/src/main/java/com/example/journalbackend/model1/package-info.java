153ea7b06f7c43f26d50579c247d3350b96906cc
kowshikreddy401 <bonthakowshikreddy@gmail.com>
2025-10-08 08:09:24 +0530

    Delete journal-backend directory

--- journal-backend/src/main/java/com/example/journalbackend/model1/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.example.journalbackend.model1;
--- journal-backend/src/main/java/com/example/journalbackend/repository/JournalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.journalbackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.journalbackend.model.Journal;
-
-public interface JournalRepository extends JpaRepository<Journal, Long> {
-}
